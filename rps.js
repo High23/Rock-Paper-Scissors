0d79a226b93e5d04c8a6ab56a7c7f9ba01a5628f
High23 <warenasb2@gmail.com>
2023-05-22 02:54:38 -0700

    Added running scoreboard and simplified old code

--- rps.js
+++ rps.js
@@ -4,29 +4,45 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === 'Rock' && computerSelection === 'Scissors'){
-        return ("You Win! Rock Beats Scissors");
-    } else if (playerSelection === 'Paper' && computerSelection === 'Rock'){
-        return ("You Win! Paper Beats Rock");
-    } else if (playerSelection === 'Scissors' && computerSelection === 'Paper'){
-        return ("You Win! Scissors Beats Paper");
-    } else if(playerSelection === 'Rock' && computerSelection === 'Paper'){
-        return ("You Lose! Paper Beats Rock");
-    } else if (playerSelection === 'Paper' && computerSelection === 'Scissors'){
-        return ("You Lose! Scissors Beats Paper");
-    } else if (playerSelection === 'Scissors' && computerSelection === 'Rock'){
-        return ("You Lose! Rock Beats Scissors");
+    if (playerSelection === computerSelection){
+        return ("Tied!");
+    } else if (playerSelection == 'Rock' && computerSelection == 'Paper' || playerSelection == 'Paper' && computerSelection == 'Scissors' 
+    || playerSelection == 'Scissors' && computerSelection == 'Rock'){
+        computerWins += 1;
+        if (computerWins === 5){
+            computerWins = 0;
+            playerWins = 0;
+            return "You Lost! The computer won 5 points before you did. :(";
+        }
+        return (`You Lose! ${playerSelection} doesn't beat ${computerSelection}`)
     } else {
-        return (`You Tied! ${playerSelection} doesn't beat ${computerSelection}`);
+        playerWins += 1;
+        if (playerWins === 5){
+            computerWins = 0;
+            playerWins = 0;
+            return "You Win! You won 5 points before the computer";
+        } 
+        return (`You Win! ${playerSelection} Beats ${computerSelection}`);
     }
 }
-const container = document.querySelector('#results');
+let playerWins = 0;
+let computerWins = 0;
+const results = document.querySelector('#results');
+const scoreboard = document.querySelector('#scoreboard');
+const playerScore = document.createElement('span');
+const computerScore = document.createElement('span');
+
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
 
     button.addEventListener('click', () => {
         let playerSelection = button.id;
         let computerSelection = getComputerChoice();
-        container.textContent = playRound(playerSelection, computerSelection);
+        results.textContent = playRound(playerSelection, computerSelection);
+        playerScore.textContent = `Player score: ${playerWins} `;
+        computerScore.textContent = `Computer score: ${computerWins}`;
     });
 });
+
+scoreboard.appendChild(playerScore);
+scoreboard.appendChild(computerScore);
